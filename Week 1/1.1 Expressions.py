9a1de7a6a78beeb89acb5897a99ef39b66b01af4
Stan Haakman <stanhaakman@icloud.com>
2021-09-06 16:18:56 +0200

    Testen van de toegang

--- Week 1/1.1 Expressions.py
+++ Week 1/1.1 Expressions.py
@@ -4,3 +4,5 @@ print ("Expressie", "5 % 2", "uitkomst", 5 % 2,"type", type(5 % 2))
 print ("Expressie","5 > 1", "uitkomst", 5 > 1, "type", type(5 > 1))
 print ("Expressie", "'5'","uitkomst", "'5'","type", type('5'))
 print ("Expressie", "5 * 2", "uitkomst", 5 * 2,"type", type(5 * 2))
+
+print('Permission test')
