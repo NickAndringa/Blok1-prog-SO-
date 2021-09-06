166c43bbf564348c9cdada0f5c374b6e42d2a073
nickandringa <n.andringa@outlook.com>
2021-09-06 16:09:01 +0200

    Tabel van opdracht 1.1 uitgewerkt in code.

---
+++ Week 1/1.1 Expressions.py
@@ -0,0 +1,6 @@
+print ("Expressie",5, "uitkomst" ,5, "type", type(5))
+print ("Expressie",5.0, "uitkomst", 5.0,"type", type(5.0))
+print ("Expressie", "5 % 2", "uitkomst", 5 % 2,"type", type(5 % 2))
+print ("Expressie","5 > 1", "uitkomst", 5 > 1, "type", type(5 > 1))
+print ("Expressie", "'5'","uitkomst", "'5'","type", type('5'))
+print ("Expressie", "5 * 2", "uitkomst", 5 * 2,"type", type(5 * 2))
